1e080233f3a7396fcf3b795c323dd216932ada72
1dayIwillbe <patel311278@gmail.com>
2021-11-14 23:21:50 +0000

    week 11 homework

--- src/homework2/NopCom2.java
+++ src/homework2/NopCom2.java
@@ -1,11 +1,14 @@
 package homework2;
 
 import homework1.BaseClass;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+
+import java.time.LocalDateTime;
 
 /**
  *
@@ -21,24 +24,27 @@ public class NopCom2 extends BaseClass {
 }
 @Test
     public void one(){
+    LocalDateTime now = LocalDateTime.now();
     driver.findElement(By.linkText("Register")).click();
     driver.findElement(By.id("gender-male")).click();   ////input[@id='gender-male']
     driver.findElement(By.id("FirstName")).sendKeys("Jignesh");
     driver.findElement(By.id("LastName")).sendKeys("Patel");
     Select day = new Select(driver.findElement(By.name("DateOfBirthDay"))); ////select[@name='DateOfBirthDay']
-    day.selectByVisibleText("31");
+    day.selectByVisibleText("12");
     Select month = new Select(driver.findElement(By.name("DateOfBirthMonth")));
     month.selectByVisibleText("December");
     Select year = new Select(driver.findElement(By.name("DateOfBirthYear")));
-    year.selectByVisibleText("1978");
-    driver.findElement(By.id("Email")).sendKeys("hello@gmail.com");
+    year.selectByVisibleText("1968");
+    driver.findElement(By.id("Email")).sendKeys("hello1"+now.getSecond()+"@gmail.com");
     driver.findElement(By.id("Company")).sendKeys("Prime Testing co.");
     WebElement chkBox = driver.findElement(By.id("Newsletter"));
     chkBox.click();
     driver.findElement(By.id("Password")).sendKeys("12345678@");
     driver.findElement(By.id("ConfirmPassword")).sendKeys("12345678@");
     driver.findElement(By.id("register-button")).click();
-
+    String expectedResult = "Your registration completed";
+    String actualResult = driver.findElement(By.xpath("//div[text()='Your registration completed']")).getText();
+    Assert.assertEquals("You are not registered",expectedResult,actualResult);
 }
 
 }
