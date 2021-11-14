248239d112747897c03c7473120219bcfb82fdf4
1dayIwillbe <patel311278@gmail.com>
2021-11-14 22:07:20 +0000

    week 11 homework

---
+++ src/homework2/NopCom2.java
@@ -0,0 +1,19 @@
+package homework2;
+
+import homework1.BaseClass;
+import org.junit.Before;
+import org.junit.Test;
+
+public class NopCom2 extends BaseClass {
+@Before
+    public void openWeb(){
+    String baseUrl = "https://demo.nopcommerce.com/";
+    openBrowser(baseUrl);
+
+}
+@Test
+    public void one(){
+
+}
+
+}
