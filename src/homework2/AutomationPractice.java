d51ea43303d8ba98f07033fa9d8739124f6bf1f5
1dayIwillbe <patel311278@gmail.com>
2021-11-15 10:42:17 +0000

    week 11 homework

---
+++ src/homework2/AutomationPractice.java
@@ -0,0 +1,27 @@
+package homework2;
+
+import homework1.BaseClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+public class AutomationPractice extends BaseClass {
+    @Before
+    public void page() {
+        openBrowser("http://automationpractice.com/index.php");
+
+    }
+
+    @Test
+    public void test() {
+        driver.findElement(By.xpath("//a[@class='login']")).click();
+        driver.findElement(By.id("email")).sendKeys("patel123@gmail.com");
+        driver.findElement(By.id("passwd")).sendKeys("123456@");
+        driver.findElement(By.id("SubmitLogin")).click();
+        String expectedResult = "There is 1 error";
+        String actualResult = driver.findElement(By.xpath("//p[contains(text(),'There is 1 error')]")).getText();
+        Assert.assertEquals("You are on login page", expectedResult, actualResult);
+
+    }
+}
